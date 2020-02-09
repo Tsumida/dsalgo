da737789be830f5082c7349dd0b61df5b5614004
Tsumida <elsa110249@163.com>
2020-02-09 17:14:48 +0800

    Add AdjMat and its tests.

--- src/Graph/graph.py
+++ src/Graph/graph.py
@@ -2,11 +2,17 @@
 
 from abc import ABC
 from typing import List, Tuple
-
+# =================================================================================
 # err msg
 ERR_INVALID_STORAGE_TYPE = "Error, invalid storage type: {}"
 ERR_NODE_EXISTS = "Error, node {} exists."
-ERR_EDGE_EXISTS = "Error, edge{} exists."
+ERR_EDGE_EXISTS = "Error, edge {} exists."
+# =================================================================================
+
+PHY_MAT = "matrix"
+PHY_ADJ_LIS = "adjacent lis"
+# =================================================================================
+
 
 class GraphException(Exception):
     def __init__(self, msg:str):
@@ -16,15 +22,14 @@ class GraphException(Exception):
         return self.msg
 
 class Graph:
-    PHY_MAT = "matrix"
-    PHY_ADJ_LIS = "adjacent lis"
+
 
     def __init__(self):
         self.__storage = None
         self.__type = None
 
     def set_type(self, type):
-        if type == Graph.PHY_ADJ_LIS or type == Graph.PHY_MAT:
+        if type == PHY_ADJ_LIS or type == PHY_MAT:
             self.__type = type
         else:
             raise GraphException(ERR_INVALID_STORAGE_TYPE.format(type))
@@ -37,10 +42,10 @@ class Graph:
         return self.__storage
 
     def from_list(self, lis:List[Tuple]):
-        if self.__type == Graph.PHY_ADJ_LIS:
+        if self.__type == PHY_ADJ_LIS:
             self.__storage = WeightedAdjLis.from_edge_list(lis)
-        elif self.__type == Graph.PHY_MAT:
-            raise Exception("Unfinished")
+        elif self.__type == PHY_MAT:
+            self.__storage = AdjMat.from_edge_list(lis)
         else:
             raise GraphException(ERR_INVALID_STORAGE_TYPE.format(self.__type))
         return self
@@ -102,7 +107,6 @@ class GraphStorage(ABC):
     def from_edge_list(edges:List[Tuple]):
         pass
 
-
 class WeightedAdjLis(GraphStorage):
     """
     带权的邻接表，不含多重边
@@ -137,11 +141,11 @@ class WeightedAdjLis(GraphStorage):
     def add_edge(self, u, v, w):
         # O(E)
         # if u or v not in self.__adj, add new node.
-        node = self.__adj.setdefault(u, list())
-        if any(v == nod.v for nod in node):
+        nodes = self.__adj.setdefault(u, list())
+        if any(v == nod.v for nod in nodes):
             raise GraphException(ERR_EDGE_EXISTS.format((u, v)))
 
-        node.append(WeightedAdjLis.__Edge(v, w))
+        nodes.append(WeightedAdjLis.__Edge(v, w))
         if v not in self.__adj:
             self.add_node(v)
         return self
@@ -211,12 +215,12 @@ class WeightedAdjLis(GraphStorage):
         kvs.sort(key=lambda p:p[0])
 
         for k, lis in kvs:
-            s = f"key={k}\t"
+            s = f"key={k}, "
             vs = ", ".join(f"({v}, {w})" for v, w in sorted(lis))
             print(s+vs)
 
     def storage_type(self):
-        return Graph.PHY_ADJ_LIS
+        return PHY_ADJ_LIS
 
 
     @staticmethod
@@ -231,3 +235,170 @@ class WeightedAdjLis(GraphStorage):
             g.add_edge(u, v, w)
         return g
 
+class AdjMat(GraphStorage):
+
+    def __init__(self):
+        super(AdjMat, self).__init__()
+        self.__mat = list()
+        self.__node2index = dict()
+        self.__index2node = dict()
+
+    def __edge2index(self, u, v):
+        return (self.__node2index.get(u, None),
+                self.__node2index.get(v, None))
+
+    def __assert_regular(self):
+        assert len(self.__node2index) == len(self.__mat),\
+            f"Error: node2index={self.__node2index}, mat={self.__mat}"
+
+    def __regularize(self):
+        """
+        负责删除节点后的整理工作， 保证self.__cnt
+                      del
+                      |
+                  0 1 2 3            0 1 2
+                0 1 2 7 3          0 1 2 3
+                1 2 3 9 N   ---->  1 2 3 N
+        del --- 2 None             2 3 N N
+                3 3 N 5 N
+
+        """
+
+        to_del = dict()  # 被删除的节点的下标
+        for n, i in self.__node2index.items():
+            if self.__mat[i] == None:
+                to_del[i] = n
+
+        rehash_map = dict() # label -> index
+        new_mat = []
+        for i, line in enumerate(self.__mat):
+            if line:
+                new_mat.append([x for i, x in enumerate(line) if i not in to_del])
+                rehash_map[self.__index2node[i]] = len(new_mat) - 1
+
+        self.__node2index = rehash_map
+        self.__index2node = dict()
+        for k, v in self.__node2index.items():
+            self.__index2node[v] = k
+        self.__mat = new_mat
+
+
+    def get_num_node(self):
+        return len(self.__node2index)
+
+    def get_num_edge(self):
+        # O(V^2)
+        cnt = 0
+        for line in self.__mat:
+            cnt += sum(1 for x in line if x )
+        return cnt
+
+    def get_edge(self, u, v):
+        # 返回 (u, v, w) 或者 None
+        a, b = self.__edge2index(u, v)
+        if a != None and b != None:
+            return (u, v, self.__mat[a][b])
+        else:
+            return None
+
+    def add_edge(self, u, v, w):
+        # if u or v not in G.V, add new node.
+        # if (u, v) exists, raise GraphException
+        if u not in self.__node2index:
+            self.add_node(u)
+        if v not in self.__node2index:
+            self.add_node(v)
+
+        a, b = self.__node2index[u], self.__node2index[v]
+        if self.__mat[a][b]:
+            raise GraphException(ERR_EDGE_EXISTS.format((u, v)))
+        self.__mat[a][b] = w
+        return self
+
+    def del_edge(self, u, v):
+        # 删除边并返回
+        a, b = self.__edge2index(u, v)
+        if a != None and b != None:
+            val = self.__mat[a][b]
+            if val:
+                self.__mat[a][b] = None
+                return u, v, val
+        return None
+
+    def contains_edge(self, u, v):
+        # 判断是否包含一条边(u, v)
+        a, b = self.__edge2index(u, v)
+
+        return a != None and b != None and self.__mat[a][b]
+
+    def get_neighbours(self, node):
+        # 获得点所有的出边
+        # usage: for v, w in graph.get_neighbours("z city")
+        a = self.__node2index.get(node, None)
+        if a != None:
+            return [(self.__index2node[i], v) for i, v in enumerate(self.__mat[a]) if v != None]
+        else:
+            return list()
+
+    def add_node(self, node):
+        if node in self.__node2index:
+            raise GraphException(ERR_NODE_EXISTS.format(node))
+
+        # 前置条件: 矩阵是紧密的
+        self.__assert_regular()
+        n = len(self.__mat)
+        if n == 0:
+            self.__mat.append([None])
+        else:
+            # 每一行都增加一列
+            for line in self.__mat:
+                line.append(None)
+            # 增加一行
+            self.__mat.append([None] * (n+1))
+
+        self.__node2index[node] = n
+        self.__index2node[n] = node
+        # 后置条件: 矩阵是紧密的
+        self.__assert_regular()
+
+
+    def del_node(self, node):
+        # O(--========================================
+        # 删除所有与node关联的边
+        self.__assert_regular()
+        index = self.__node2index.get(node, None)
+        if index != None:
+            self.__mat[index] = None # 标记node的行为None
+            self.__regularize()
+        self.__assert_regular()
+
+
+    def set_weight(self, u, v, new_weight):
+        a, b = self.__edge2index(u, v)
+
+        if a != None and b != None:
+            self.__mat[a][b] = new_weight
+        return self
+
+    def contains_node(self, node):
+        return node in self.__node2index
+
+    def print_status(self):
+        kis = [(k, v) for k, v in self.__node2index.items()]
+        kis.sort(key=lambda x:x[1])
+
+        for k, i in kis:
+            print(f"key={k} -> index={i}")
+
+        for line in self.__mat:
+            print(", ".join(str(e) if e != None else "___" for e in line ))
+
+    def storage_type(self):
+        return PHY_MAT
+
+    @staticmethod
+    def from_edge_list(edges: List[Tuple]):
+        g = AdjMat()
+        for u, v, w in edges:
+            g.add_edge(u, v, w)
+        return g
--- src/Graph/test_graph.py
+++ src/Graph/test_graph.py
@@ -2,7 +2,7 @@
 
 from unittest import TestCase
 
-from src.Graph.graph import WeightedAdjLis, Graph, GraphException
+from src.Graph.graph import WeightedAdjLis, Graph, GraphException, AdjMat, PHY_MAT, PHY_ADJ_LIS
 
 CASES = {
         "tree": [
@@ -28,21 +28,44 @@ CASES = {
             (4, 1, 1), (4, 2, 1), (4, 3, 1),
         ],
     }
+
+Citys = [
+    ("A city", "B city", 2.2),
+    ("A city", "C city", 1.3),
+    ("A city", "D city", 4.5),
+    ("B city", "D city", 3.2),
+    ("C city", "F city", 10.7),
+    ("F city", "E city", 20.1),
+    ("E city", "B city", 4.9),
+]
+
 class TestGraph(TestCase):
-    
+
     def test_usage(self):
         lis = CASES["tree"]
-        _ = Graph()\
-            .set_type(Graph.PHY_ADJ_LIS)\
-            .from_list(lis)
+        _ = Graph().set_type(PHY_ADJ_LIS).from_list(lis)
+        _ = Graph.from_edge_list(PHY_ADJ_LIS, lis)
 
-        _ = Graph.from_edge_list(Graph.PHY_ADJ_LIS, lis)
+        _ = Graph().set_type(PHY_MAT).from_list(lis)
+        _ = Graph.from_edge_list(PHY_MAT, lis)
 
     def test_graph_storage(self):
         lis = CASES["tree"]
-        g = Graph.from_edge_list(Graph.PHY_ADJ_LIS, lis)
+        g = Graph.from_edge_list(PHY_ADJ_LIS, lis)
         sto = g.get_storage()
         assert isinstance(sto, WeightedAdjLis)
+
+        g2 = Graph.from_edge_list(PHY_MAT, lis)
+        sto2 = g2.get_storage()
+        assert isinstance(sto2, AdjMat)
+
+    def test_city(self):
+        g1 = Graph.from_edge_list(PHY_MAT, Citys)
+        g2 = Graph.from_edge_list(PHY_ADJ_LIS, Citys)
+
+        g1.get_storage().print_status()
+        print("-"*32)
+        g2.get_storage().print_status()
 
 
 class TestWeightedAdjLis(TestCase):
@@ -183,3 +206,140 @@ class TestWeightedAdjLis(TestCase):
         g.set_weight(666, 233, 888)
         assert None == g.get_edge(666, 233)
 
+class TestAdjMat(TestCase):
+
+    def test_add_node(self):
+        g1 = AdjMat()
+        g1.add_node(1)
+        assert g1.get_num_node() == 1
+        assert g1.contains_node(1)
+
+        g1.add_node(2)
+        assert g1.get_num_node() == 2
+        assert g1.contains_node(2)
+
+        try:
+            g1.add_node(2)
+            raise Exception("Test should panic.")
+        except GraphException:
+            pass
+
+
+    def test_add_edge(self):
+        g1 = AdjMat()
+        g1.add_edge(1, 2, 3)
+        assert g1.get_num_edge() == 1
+        assert g1.get_num_node() == 2
+
+        g1.add_edge(2, 1, 4)
+        assert g1.get_num_edge() == 2
+        assert g1.get_num_node() == 2
+
+        try:
+            g1.add_edge(1, 2, 1)
+            raise Exception("Test should panic.")
+        except GraphException:
+            pass
+
+    def test_get_neighbour(self):
+        g = AdjMat()\
+            .add_edge(1, 2, 2)\
+            .add_edge(2, 3, 4)\
+            .add_edge(1, 3, 5)\
+            .add_edge(2, 7, 3)
+
+        n1 = g.get_neighbours(1)
+        n1.sort(key=lambda x:x[0])
+        assert n1 == [(2, 2), (3, 5)]
+
+        n2 = g.get_neighbours(2)
+        n2.sort(key=lambda x:x[0])
+        assert n2 == [(3, 4), (7, 3)]
+
+        assert [] == g.get_neighbours(666)
+
+    def test_get_edge(self):
+        for label, lis in CASES.items():
+            g = AdjMat.from_edge_list(lis)
+            for u, v, w in lis:
+                p = g.get_edge(u, v)
+                assert (u, v, w) == p, \
+                    f"error, test case: {label}. expected ({u}, {v}, {w}), got {p}"
+
+    def test_delete_node(self):
+        g1 = AdjMat.from_edge_list(CASES["circle"])
+        assert g1.get_num_node() == 5
+        assert g1.get_num_edge() == 5
+
+        g1.del_node(1)
+        assert g1.get_num_node() == 4
+        assert g1.get_num_edge() == 3
+
+        g1.del_node(2)
+        assert g1.get_num_node() == 3
+        assert g1.get_num_edge() == 2
+
+        g1.del_node(3)
+        assert g1.get_num_node() == 2
+        assert g1.get_num_edge() == 1
+
+        g1.del_node(4)
+        assert g1.get_num_node() == 1
+        assert g1.get_num_edge() == 0
+
+        g1.del_node(5)
+        assert g1.get_num_node() == 0
+        assert g1.get_num_edge() == 0
+
+        g1.del_node(2)
+        assert g1.get_num_node() == 0
+        assert g1.get_num_edge() == 0
+
+    def test_delete_edge(self):
+        """
+        "4-complete":[
+            (1, 2, 1), (1, 3, 1), (1, 4, 1),
+            (2, 1, 1,),(2, 3, 1), (2, 4, 1),
+            (3, 1, 1), (3, 2, 1), (3, 4, 1),
+            (4, 1, 1), (4, 2, 1), (4, 3, 1),
+        ],
+        :return:
+        """
+        g = AdjMat.from_edge_list(CASES["4-complete"])
+        assert g.del_edge(1, 2) == (1, 2, 1)
+        assert g.del_edge(2, 1) == (2, 1, 1)
+        assert g.get_num_edge() == 10, f"error: {g.get_num_edge()}"
+        assert g.get_num_node() == 4
+        assert g.del_edge(1, 2) == None
+
+
+    def test_from_lis(self):
+        for label, lis in CASES.items():
+            node_set = set()
+            for u, v, _ in lis:
+                node_set.add(u)
+                node_set.add(v)
+            n = len(lis)
+
+            g = AdjMat.from_edge_list(lis)
+
+            assert g.get_num_edge() == n
+            assert g.get_num_node() == len(node_set)
+            for u, v, w in lis:
+                assert g.contains_node(u)
+                assert g.contains_node(v)
+                assert (u, v, w) == g.get_edge(u, v)
+
+    def test_set_weight(self):
+        g = AdjMat.from_edge_list(CASES["4-complete"])
+
+        assert (2, 1, 1) == g.get_edge(2, 1)
+        assert (1, 2, 1) == g.get_edge(1, 2)
+
+        g.set_weight(1, 2, 10).set_weight(2, 1, 666)
+        assert (2, 1, 666) == g.get_edge(2, 1)
+        assert (1, 2, 10) == g.get_edge(1, 2)
+
+        assert None == g.get_edge(666, 233)
+        g.set_weight(666, 233, 888)
+        assert None == g.get_edge(666, 233)
